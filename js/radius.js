f005397469b9523e2405c641e3068892554bf27d
eriq <perik_92@hotmail.com>
2020-04-09 22:35:27 -0500

    Creacion del repo

---
+++ js/radius.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){ 	
+						   
+						   
+	
+	$('.menu_nav ul li a').css({"border-radius":"8px", "-moz-border-radius":"8px", "-webkit-border-radius":"8px"});
+	$('p.infopost').css({"border-radius":"8px", "-moz-border-radius":"8px", "-webkit-border-radius":"8px"});
+	$('.content_resize').css({"border-radius":"12px", "-moz-border-radius":"12px", "-webkit-border-radius":"12px"});
+	
+	 
+});	
